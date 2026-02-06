7216279c2d0c9203c3dbb7ea22720dfcaa272dad
mohammadoumar <umer.mushtaq@u-paris2.fr>
2026-02-06 18:40:13 +0100

    add argparse to main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import argparse
+
 def main():
-    print("Hello from mdlt-er!")
+    argparser = argparse.ArgumentParser(description="mdlt-er: A tool for emotion recognition.")
+    argparser.add_argument("--input", type=str, help="Path to the input data.")
+    
+    filename = argparser.parse_args().input
+    print("Hello from mdlt-er! \nYou provided the input file: " + filename)
 
 
 if __name__ == "__main__":
